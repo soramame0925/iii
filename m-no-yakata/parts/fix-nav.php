13ba0f39d2de4b4503e8d71408df37a1824c1628
soramame0925 <sora1968924391014@gmail.com>
2025-12-03 13:29:12 +0900

    Add files via upload

--- m-no-yakata/parts/fix-nav.php
+++ m-no-yakata/parts/fix-nav.php
@@ -1,7 +1,7 @@
 <nav id="fix_bottom_menu" class="mno-fix-nav" role="navigation" aria-label="Bottom Navigation">
   <ul class="mno-fix-nav__list">
     <li class="mno-fix-nav__item"><a href="/" class="mno-fix-nav__btn">🏠<span>ホーム</span></a></li>
-    <li class="mno-fix-nav__item"><button type="button" class="mno-fix-nav__btn">🔎<span>フィルター</span></button></li>
+    <li class="mno-fix-nav__item"><a href="#" class="mno-fix-nav__btn" id="mno-filter-btn">🔎<span>フィルター</span></a></li>
 <li class="mno-fix-nav__item">
   <a href="<?php echo esc_url( home_url('/discover/') ); ?>" class="mno-fix-nav__btn">
     🎞️<span>ショート</span>
